aaf35113640609df09f7af88dc9a46fdc4d39d84
hiroshiimaizumi <hiroshiimaizumi0611@gmail.com>
2022-11-10 12:26:30 +0900

    fix Program.cs

--- Program.cs
+++ Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using MementoPattern;
+
+var calc = new Calc();
+for (int n = 1; n <= 5; n++)
+{
+    calc.Add(n);
+}
+
+Console.WriteLine($"First Calc 現在の数値は : {calc.temperatureNumber} です。");
+SnapShot.AddSnapShot("First Calc", calc.CreateMemento());
+
+
+var calcVersion2 = new Calc();
+calcVersion2.SetMemento(SnapShot.GetSnapShot("First Calc"));
+for (var n = 6; n <= 10; n++)
+{
+    calcVersion2.Add(n);
+}
+
+Console.WriteLine($"Second Calc 現在の数値は : {calcVersion2.temperatureNumber} です。");
+SnapShot.AddSnapShot("Second Calc", calcVersion2.CreateMemento());
+
+var calcVersion3 = new Calc();
+calcVersion3.SetMemento(SnapShot.GetSnapShot("Second Calc"));
+
+Console.WriteLine($"Third Calc 現在の数値は : {calcVersion3.temperatureNumber} です。");
